e2e3150e95675a226c943dfada6708a779603800
happytoday <964033138@qq.com>
2017-09-05 17:56:08 +0800

    个人信息

---
+++ src/pages/grzx/yhzcType.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--注册用户-->
+  <div class="wrapper">
+    <div class="input-info">
+
+      <div class="user-type">
+        <router-link :to="{path:'/yhzcPerson'}" tag="div">
+          <div class="person-user">个人用户
+            <i class="icon-enter iconfont icon-style"></i>
+          </div>
+        </router-link>
+
+        <div class="comm-user">企业用户
+          <i class="icon-enter iconfont icon-style"></i>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<style rel="stylesheet/scss" lang="scss">
+  @import '../../assets/sass/_base';
+  .wrapper {
+    overflow: hidden;
+    .user-type{
+      background: #ffffff;
+      vertical-align: middle;
+      .person-user{
+        height: 50px;
+        line-height: 50px;
+        border-bottom: 1px solid $border-color;
+        padding-left: 40px;
+      }
+      .comm-user{
+        height: 50px;
+        line-height: 50px;
+        padding-left: 40px;
+      }
+      .icon-style{
+        color: $theme-color;
+        float: right;
+        padding-right: 10px;
+      }
+
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,11 @@ import Hello from '@/components/Hello'
 
 import index from '@/pages/index'
 import fxbb from '@/pages/fxbb'
-import grxx from '@/pages/grxx'
+import grxx from '@/pages/grzx/grxx'
 import map from '@/pages/map'
+import yhzcTel from '@/pages/grzx/yhzcTel'
+import yhzcCode from '@/pages/grzx/yhzcCode'
+import yhzcType from '@/pages/grzx/yhzcType'
 
 Vue.use(Router)
 
@@ -28,6 +31,19 @@ export default new Router({
       path: '/map',
       name: 'map',
       component: map
+    },{
+      path: '/yhzcTel',
+      name: 'yhzcTel',
+      component: yhzcTel
+    },{
+      path: '/yhzcCode',
+      name: 'yhzcCode',
+      component: yhzcCode
+    },{
+      path: '/yhzcType',
+      name: 'yhzcType',
+      component: yhzcType
     }
+
   ]
 })
